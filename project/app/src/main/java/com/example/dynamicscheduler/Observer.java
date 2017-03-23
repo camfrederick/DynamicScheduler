28e7ed9b169393a8cbf18ad3facf2ad2c055951b
jryanofarrell <jryanofarrell@utexas.edu>
2017-03-23 16:22:14 -0600

    removed Observer from head

--- project/app/src/main/java/com/example/dynamicscheduler/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.example.dynamicscheduler;
-
-/**
- * Created by jryanofarrell on 3/23/2017.
- */
-
-public interface Observer  {
-    //public void update(int startTime, int stopTime, String title, String location);
-}
