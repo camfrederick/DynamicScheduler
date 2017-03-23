7d0b31d93326e855ac18d86adabfccf2695865ea
cc2222 <casey.cotter@utexas.edu>
2017-03-23 15:23:11 -0500

    added Schedule and BusyTime classes

--- project/app/src/main/java/com/example/dynamicscheduler/BusyTime.java
+++ project/app/src/main/java/com/example/dynamicscheduler/BusyTime.java
@@ -5,4 +5,50 @@ package com.example.dynamicscheduler;
  */
 
 public class BusyTime {
+
+    private int starttime, stoptime;
+    private String days, title;
+
+    public BusyTime(){
+
+    }
+
+    public BusyTime(int start, int stop, String repeat, String name){
+        starttime = start;
+        stoptime = stop;
+        days = repeat;
+        title = name;
+    }
+
+    public int getStarttime(){
+        return starttime;
+    }
+
+    public int getStoptime(){
+        return stoptime;
+    }
+
+    public String getDays(){
+        return days;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setStarttime(int start){
+        starttime = start;
+    }
+
+    public void setStoptime(int stop){
+        stoptime = stop;
+    }
+
+    public void setDays(String repeat){
+        days = repeat;
+    }
+
+    public void setTitle(String name){
+        title = name;
+    }
 }
--- project/app/src/main/java/com/example/dynamicscheduler/Schedule.java
+++ project/app/src/main/java/com/example/dynamicscheduler/Schedule.java
@@ -1,8 +1,34 @@
 package com.example.dynamicscheduler;
 
+import java.util.ArrayList;
+
 /**
  * Created by Cam on 3/23/2017.
  */
 
 public class Schedule {
+
+    private ArrayList<Event> events;
+    private ArrayList<BusyTime> busyTimes;
+
+    public Schedule(){
+
+    }
+
+    public void addEvent(Event e){
+        events.add(e);
+    }
+
+    public void removeEvent(Event e){
+        events.remove(e);
+    }
+
+    public ArrayList<Event> getEvents(){
+        return events;
+    }
+
+    public ArrayList<BusyTime> getBusyTimes(){
+        return busyTimes;
+    }
+
 }
