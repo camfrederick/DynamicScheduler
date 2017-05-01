3b741423e3032ea16930e8d81daacba40deacebb
cc2222 <casey.cotter@utexas.edu>
2017-04-30 22:06:58 -0500

    changed Junit test to use currentDate

--- project/app/src/test/java/com/example/dynamicscheduler/ExampleUnitTest.java
+++ project/app/src/test/java/com/example/dynamicscheduler/ExampleUnitTest.java
@@ -18,33 +18,41 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void createEventTest(){
+        DateTime now = new DateTime(System.currentTimeMillis());
+        String complexdate = now.toString();
+        String currentDate = complexdate.substring(0,complexdate.indexOf("T"));
+
         User user = new User("tester","123madeup","jryanofarrell@gmail.com","5128978944");
-        user.createEvent("shopping",1600,1700,"HEB","4/24/2017");
-        user.createEvent("eating",1900,2000,"Home","4/24/2017");
-        user.createEvent("studying",2000,2200,"Home","4/24/2017");
+        user.createEvent("shopping",1600,1700,"HEB",currentDate);
+        user.createEvent("eating",1900,2000,"Home",currentDate);
+        user.createEvent("studying",2000,2200,"Home",currentDate);
 
         ArrayList<Event> eventlist = user.getSchedule().getEvents();
         assertEquals(eventlist.get(0).getStartTime(),1600);
         assertEquals(eventlist.get(0).getStopTime(),1700);
         assertEquals(eventlist.get(1).getTitle(),"eating");
         assertEquals(eventlist.get(1).getLocation(),"Home");
-        assertEquals(eventlist.get(2).day,24);
-        assertEquals(eventlist.get(2).month,4);
-        assertEquals(eventlist.get(2).year,2017);
+        assertEquals(eventlist.get(2).day,parseDate(currentDate)[2]);
+        assertEquals(eventlist.get(2).month,parseDate(currentDate)[1]);
+        assertEquals(eventlist.get(2).year,parseDate(currentDate)[0]);
 
-        eventlist.get(0).changeEvent("shopping", 1400, 1600, "HEB", "4/24/2017");
-        eventlist.get(1).changeEvent("dinner with Ryan", 1900, 2000, "Canes", "4/24/2017");
-        eventlist.get(2).changeEvent("studying", 2000, 2200, "Home", "4/25/2017");
+        eventlist.get(0).changeEvent("shopping", 1400, 1600, "HEB", currentDate);
+        eventlist.get(1).changeEvent("dinner with Ryan", 1900, 2000, "Canes", currentDate);
+        eventlist.get(2).changeEvent("studying", 2000, 2200, "Home", addDays(currentDate, 1));
 
         assertEquals(eventlist.get(0).getStartTime(), 1400);
         assertEquals(eventlist.get(0).getStopTime(), 1600);
         assertEquals(eventlist.get(1).getTitle(), "dinner with Ryan");
         assertEquals(eventlist.get(1).getLocation(), "Canes");
-        assertEquals(eventlist.get(2).day, 25);
+        assertEquals(eventlist.get(2).day, parseDate(addDays(currentDate, 1))[2]);
     }
 
     @Test
     public void createGroupEventTest(){
+        DateTime now = new DateTime(System.currentTimeMillis());
+        String complexdate = now.toString();
+        String currentDate = complexdate.substring(0,complexdate.indexOf("T"));
+
         Member user1 = new Member("Alice","123 Rio Grande","alice@gmail.com","5128978944");
         Member user2 = new Member("Blake","234 Pearl","blake@gmail.com","5128453434");
         Member user3 = new Member("Casey","123 Nueces","casey@gmail.com","5125551234");
@@ -54,19 +62,19 @@ public class ExampleUnitTest {
         members.add(user1);
         g.addMembers(members);
 
-        user3.createGroupEvent(g, "Work on Project", 1200, 1400, "PCL", "4/25/2017");
+        user3.createGroupEvent(g, "Work on Project", 1200, 1400, "PCL", currentDate);
 
         assertEquals(user2.getSchedule().getEvents().get(0).getStartTime(), 1200);
         assertEquals(user1.getSchedule().getEvents().get(0).getTitle(), "Work on Project");
         assertEquals(user3.getSchedule().getEvents().get(0).getStopTime(),  1400);
 
         GroupEvent gevent = (GroupEvent)user3.getSchedule().getEvents().get(0);
-        gevent.changeEvent("Project meeting", 1300, 1500, "PCL", "4/26/2017");
+        gevent.changeEvent("Project meeting", 1300, 1500, "PCL", addDays(currentDate,1));
 
         assertEquals(user2.getSchedule().getEvents().get(0).getStartTime(), 1300);
         assertEquals(user1.getSchedule().getEvents().get(0).getTitle(), "Project meeting");
         assertEquals(user3.getSchedule().getEvents().get(0).getStopTime(),  1500);
-        assertEquals(user2.getSchedule().getEvents().get(0).day, 26);
+        assertEquals(user2.getSchedule().getEvents().get(0).day, parseDate(addDays(currentDate,1))[2]);
     }
 
     @Test
