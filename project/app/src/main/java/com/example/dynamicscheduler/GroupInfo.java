85c425e0749762bc49e0981968595d0cdd02e26b
rohankonde <rohankonde@yahoo.com>
2017-05-04 12:07:30 -0500

    add members works

--- project/app/src/main/java/com/example/dynamicscheduler/GroupInfo.java
+++ project/app/src/main/java/com/example/dynamicscheduler/GroupInfo.java
@@ -1,10 +1,14 @@
 package com.example.dynamicscheduler;
 
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,7 +17,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GroupInfo extends AppCompatActivity {
     FirebaseUser user;
@@ -23,6 +31,8 @@ public class GroupInfo extends AppCompatActivity {
     ListView groupList;
     FloatingActionButton addmember;
 
+    String m_Text;
+    String group_name;
     Group selected_group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +43,7 @@ public class GroupInfo extends AppCompatActivity {
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         db = FirebaseDatabase.getInstance();
-        String group_name = getIntent().getStringExtra("group_name");
+        group_name = getIntent().getStringExtra("group_name");
 
 
 
@@ -60,6 +70,82 @@ public class GroupInfo extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+        addmember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(GroupInfo.this);
+                builder.setTitle("Add New Member:");
+
+                final EditText input = new EditText(GroupInfo.this);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        m_Text = input.getText().toString();
+
+                       // groupEntry.put(m_Text, newgroup);
+                        dataRef = db.getReference("users");
+                        dataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                                    User currentUser = ds.getValue(User.class);
+                                    currentUser.updateGroupNames(ds);
+                                    if(currentUser.getEmail().equals(m_Text)){
+                                        currentUser.group_IDs.add(group_name);
+                                        Map<String, Object> userupdate = new HashMap<String, Object>();
+                                        userupdate.put(currentUser.getUserID(), currentUser);
+                                        db.getReference("users").updateChildren(userupdate);
+                                        break;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+
+                        dataRef = db.getReference("groups");
+                        dataRef.child(group_name).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                Group updateGroup = dataSnapshot.getValue(Group.class);
+                                updateGroup.updateMemberDatabase(dataSnapshot);
+                                updateGroup.member_names.add(m_Text);
+                                Map<String, Object> groupEntry = new HashMap<String, Object>();
+                                groupEntry.put(group_name, updateGroup);
+                                db.getReference("groups").updateChildren(groupEntry);
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+                        groupList.setAdapter(adapter);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
             }
         });
 
